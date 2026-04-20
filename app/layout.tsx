ee53f21dfe3e1d2b8639e5a3091c9b2d4cad90b7
nikhilbt1997 <nikhilbt1997@gmail.com>
2026-04-20 17:49:22 +0530

    fix layout fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700&family=DM+Sans:wght@300;400;500&display=swap" rel="stylesheet" />
       </head>
       <body style={{ margin: 0, padding: 0, background: '#0a0f0d' }}>
